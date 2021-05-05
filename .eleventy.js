70d406ca64984424b6f0e6d37da4ce0c8fe41316
Thilina Pradeep <thilinatpr@gmail.com>
2021-05-05 20:09:37 +0530

    Template Improved

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,15 @@
-module.exports = {
+module.exports = function(eleventyConfig) {
+    eleventyConfig.addPassthroughCopy('assets');
+    eleventyConfig.setFrontMatterParsingOptions({
+        excerpt: true,
+        // Optional, default is "---"
+        excerpt_separator: "<!-- excerpt -->"
+      });
+    
+    return {
     dir: {
         includes: "_includes",
         layouts: "_includes/layouts"
     }
-}
+    };
+};
